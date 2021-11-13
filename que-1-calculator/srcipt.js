fdc3fa451058761142ac732fbd7bd3ca272875f4
Harshit Paliwal <harshitpaliwal95@gmail.com>
2021-11-13 15:46:41 +0530

    optimise 1que

--- que-1-calculator/srcipt.js
+++ que-1-calculator/srcipt.js
@@ -1,46 +1,17 @@
 let result = document.querySelector(".result");
-function plus() {
-  let numOne = document.getElementById("numOne");
+function calc(type) {
+  let numOne = document.querySelector("#numOne");
   numOne = Number(numOne.value);
-
-  let numTwo = document.getElementById("numTwo");
+  let numTwo = document.querySelector("#numTwo");
   numTwo = Number(numTwo.value);
 
-  let plus = numOne + numTwo;
-
-  result.innerHTML = plus;
-}
-function minus() {
-  let numOne = document.getElementById("numOne");
-  numOne = Number(numOne.value);
-
-  let numTwo = document.getElementById("numTwo");
-  numTwo = Number(numTwo.value);
-
-  let minus = numOne - numTwo;
-
-  result.innerHTML = Math.abs(minus);
-}
-
-function multiply() {
-  let numOne = document.getElementById("numOne");
-  numOne = Number(numOne.value);
-
-  let numTwo = document.getElementById("numTwo");
-  numTwo = Number(numTwo.value);
-
-  let multiply = numOne * numTwo;
-
-  result.innerHTML = multiply;
-}
-function divide() {
-  let numOne = document.getElementById("numOne");
-  numOne = Number(numOne.value);
-
-  let numTwo = document.getElementById("numTwo");
-  numTwo = Number(numTwo.value);
-
-  let divide = numOne / numTwo;
-
-  result.innerHTML = divide;
+  if (type === "plus") {
+    result.innerHTML = numOne + numTwo;
+  } else if (type === "minus") {
+    result.innerHTML = Math.abs(numOne - numTwo);
+  } else if (type === "multiply") {
+    result.innerHTML = numOne * numTwo;
+  } else {
+    result.innerHTML = numOne / numTwo;
+  }
 }
