be4b74e1ab052dbdc59e06048e12dacbd2c0f45f
Harshit Paliwal <harshitpaliwal95@gmail.com>
2021-12-01 10:19:33 +0530

    last push

---
+++ again/app.js
@@ -0,0 +1,33 @@
+// let output = document.querySelector(".output");
+
+// let loginBtn = document.getElementById("login-btn");
+
+// loginBtn.addEventListener("click", clickHandler);
+
+// let userPass = document.querySelector("#userPassword");
+// userPass.addEventListener("input", clickHandler);
+
+// function clickHandler() {
+//   {
+//     var userName = document.querySelector("#userName");
+//     var userPass = document.querySelector("#userPassword");
+//     userName = userName.value;
+//     userPass = userPass.value;
+
+//     if (userPass.includes(userName)) {
+//       output.innerHTML = "you can't write name is your passcode";
+//       output.style.color = "red";
+//     } else {
+//       output.innerHTML = "success";
+//       output.style.color = "green";
+//     }
+//   }
+// }
+
+// let btn = document.querySelector(".btn");
+
+// btn[1].addEventListener("click", () => {
+//   console.log("ok");
+// });
+
+// node list
